fc70efde8645cafb6cb9696ad8186d950e8a85db
gengjiawen <technicalcute@gmail.com>
2017-07-30 16:33:06 +0800

    another try

--- script/docker-build.js
+++ script/docker-build.js
@@ -4,7 +4,7 @@ let cmd = 'npm install && npm run release'
 
 if (process.platform === 'linux') {
   cmd =
-    'docker run --rm -i -v ${PWD}:/project -v ${PWD##*/}-node-modules:/project/node_modules -v ~/.electron:/root/.electron electronuserland/electron-builder /bin/bash -c "apt update && apt install libsecret-1-dev && npm install && npm run release"'
+    'docker run --rm -i -v ${PWD}:/project -v ${PWD##*/}-node-modules:/project/node_modules -v ~/.electron:/root/.electron electronuserland/electron-builder /bin/bash -c "sudo apt-get update && sudo apt-get install libsecret-1-dev && npm install && npm run release"'
 }
 
 console.log(`executing ${cmd}`)
