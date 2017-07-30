06160fb71413a30f17f2179715f586eac6d20431
gengjiawen <technicalcute@gmail.com>
2017-07-30 16:08:11 +0800

    try yarn

--- script/docker-build.js
+++ script/docker-build.js
@@ -3,7 +3,7 @@ const exec = require('child_process').execSync
 let cmd = 'npm run release'
 
 if (process.platform === 'linux') {
-  cmd = "docker run --rm -i -v ${PWD}:/project -v ${PWD##*/}-node-modules:/project/node_modules -v ~/.electron:/root/.electron electronuserland/electron-builder /bin/bash -c \"npm run release\""
+  cmd = "docker run --rm -i -v ${PWD}:/project -v ${PWD##*/}-node-modules:/project/node_modules -v ~/.electron:/root/.electron electronuserland/electron-builder /bin/bash -c \"yarn && yarn run release\""
 }
 
 console.log(`executing ${cmd}`)
