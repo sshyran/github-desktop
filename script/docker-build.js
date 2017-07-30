2b8ebea3ca6ecde6b848d03b861f4a722a7843a6
gengjiawen <technicalcute@gmail.com>
2017-07-30 15:52:16 +0800

    add build log

--- script/docker-build.js
+++ script/docker-build.js
@@ -6,5 +6,6 @@ if (process.platform === 'linux') {
   cmd = "docker run --rm -i -v ${PWD}:/project -v ${PWD##*/}-node-modules:/project/node_modules -v ~/.electron:/root/.electron electronuserland/electron-builder /bin/bash -c \"npm install && npm run release\""
 }
 
+console.log(`excuting ${cmd}`)
 const result = exec(cmd)
 console.log(result)
