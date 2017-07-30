e0e71f9dd52b35fc0f56129afe174afb3000fb5d
gengjiawen <technicalcute@gmail.com>
2017-07-30 17:33:08 +0800

    give yarn a chance

--- script/docker-build.js
+++ script/docker-build.js
@@ -4,7 +4,7 @@ let cmd = 'npm install && npm run release'
 
 if (process.platform === 'linux') {
   cmd =
-    'docker run --rm -i -v ${PWD}:/project -v ${PWD##*/}-node-modules:/project/node_modules -v ~/.electron:/root/.electron electronuserland/electron-builder /bin/bash -c "apt-get update && apt-get install -y libsecret-1-dev && npm install && npm run release"'
+    'docker run --rm -i -v ${PWD}:/project -v ${PWD##*/}-node-modules:/project/node_modules -v ~/.electron:/root/.electron electronuserland/electron-builder /bin/bash -c "apt-get update && apt-get install -y libsecret-1-dev && yarn install && yarn run release"'
 }
 
 console.log(`executing ${cmd}`)
